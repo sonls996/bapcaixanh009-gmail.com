2dfeb6dc0157c8169bb4c6ddba3682e72a319072
Lê Hồng Sơn <bapcaixanh009@gmail.com>
2020-03-28 20:59:31 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,55 @@
+
+/**
+ * slider cho header
+ */
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 6000); 
+}
+////
+
+
+document.getElementById("btn-search").addEventListener("click",toggleSearch);
+
+function toggleSearch(){
+    if(document.getElementById("click").style.maxHeight<="0px"){
+        document.getElementById("click").style.maxHeight="40px";
+    }
+    else {
+        document.getElementById("click").style.maxHeight="0px";
+    }
+    // alert("say helol");
+}
+
+
+/// accordion  thông tin 
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
